e399837dfc2b3af5604934e3988d0a325008e2bb
Jorgelina Mariano <jorgelina.mariano@gmail.com>
2023-07-11 20:55:05 -0300

    + img

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -1,20 +1,15 @@
+import './Item.css'
 import { Link } from "react-router-dom"
 
 const Item = ({ id, nombre, precio, img }) => {
     return (
-           <div class="row row-cols-2 row-cols-md-1 g-4">
-                <div class="col">
-                    <div class="card">
-                        <img src={img} class="card-img-top" alt={nombre} />
-                        <div class="card-body">
-                            <h5 class="card-title">Nombre: {nombre}</h5>
-                            <p class="card-text">Precio: {precio}</p>
-                            <p class="card-text">Id: {id}</p>
-                            <Link to={`/item/${id}`}>Ver detalles</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div >
+            <img className="imgCard" src={img} alt={nombre} />
+            <h5 class="card-title">Nombre: {nombre}</h5>
+            <p class="card-text">Precio: {precio}</p>
+            <p class="card-text">Id: {id}</p>
+            <Link to={`/item/${id}`}>Ver detalles</Link>
+        </div>
     )
 }
 
